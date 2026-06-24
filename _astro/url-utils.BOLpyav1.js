4a6c91a1c1570b6db29e844f78001bdb410b86c6
Casumi713 <Casumi713@outlook.com>
2026-06-24 05:26:30 +0000

    Deploying to gh-pages from @ Casumi713/Casumi713.github.io@346f89e1ab370c5167066ece6a9b205cd0f633f5 🚀

--- _astro/url-utils.BOLpyav1.js
+++
@@ -1,1 +0,0 @@
-import"./translation.rNkBIhEG.js";import"./config.Dy-QsR7I.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
