bb0f324b76a5d2d258d8fe4f6ee169d13bc5921e
Casumi713 <Casumi713@users.noreply.github.com>
2026-06-24 16:10:35 +0000

    Deploying to gh-pages from @ Casumi713/Casumi713.github.io@b6225a4c0930dcb8ac5cdacd7d10451c7ff03834 🚀

---
+++ _astro/url-utils.CGmHNsAO.js
@@ -0,0 +1,1 @@
+import"./translation.vJmCNnG2.js";import"./config.BdYYN1h_.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
