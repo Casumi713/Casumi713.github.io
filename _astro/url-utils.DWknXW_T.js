53b84a34f0ea44d41bb50ae8ddc2497ac2ec1465
Casumi713 <Casumi713@outlook.com>
2026-06-24 11:05:20 +0000

    Deploying to gh-pages from @ Casumi713/Casumi713.github.io@5fd8fad8e3a62f4c9b16a3988736d8f0d03d6044 🚀

---
+++ _astro/url-utils.DWknXW_T.js
@@ -0,0 +1,1 @@
+import"./translation.6y6rxFGD.js";import"./config.D5xnA7tr.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
